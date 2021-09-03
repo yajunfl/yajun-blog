5094048172e65d6069407eaab82b91c649aa45b5
Yajun Wang <yajun01@gmail.com>
2021-09-03 13:35:05 -0400

    add env and database url

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
